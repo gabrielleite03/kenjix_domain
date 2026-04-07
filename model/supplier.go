fcdcd1f1a34ef953790d7e519aed2bcdc3792362
unknown <gabrielleite03@gmail.com>
2026-04-07 10:01:47 -0300

    created model suppliers

--- model/supplier.go
+++ model/supplier.go
@@ -1,18 +1,19 @@
 package model
 
 type Supplier struct {
-	ID         int64   `json:"id" db:"id"`
-	Name       string  `json:"name" db:"name"`
-	CNPJ       string  `json:"cnpj" db:"cnpj"`
-	Country    *string `json:"country,omitempty" db:"country"`
-	Email      *string `json:"email,omitempty" db:"email"`
-	Phone      *string `json:"phone,omitempty" db:"phone"`
-	Seller     *string `json:"seller,omitempty" db:"seller"`
-	SellerFone *string `json:"seller_fone,omitempty" db:"seller_fone"`
-	Active     bool    `json:"active" db:"active"`
-	CategoryID *int64  `json:"category_id,omitempty" db:"category_id"`
+	ID           int64   `json:"id" db:"id"`
+	RazaoSocial  string  `json:"razaoSocial" db:"razao_social"`
+	NomeFantasia string  `json:"nomeFantasia" db:"nome_fantasia"`
+	CNPJ         string  `json:"cnpj" db:"cnpj"`
+	IE           *string `json:"ie,omitempty" db:"ie"`
+	Address      *string `json:"address,omitempty" db:"address"`
+	Salesperson  *string `json:"salesperson,omitempty" db:"sales_person"`
+	Email        *string `json:"email,omitempty" db:"email"`
+	Phone        *string `json:"phone,omitempty" db:"phone"`
+	Active       bool    `json:"active" db:"active"`
+	CategoryID   *int64  `json:"category_id,omitempty" db:"category_id"`
 
-	Category *Category `json:"category,omitempty"`
+	Category *Category `json:"category"`
 }
 
 func (s *Supplier) IsActive() bool {
