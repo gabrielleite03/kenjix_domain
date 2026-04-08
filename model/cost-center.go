125b2bfa12e92e9fc4c091984a3dca7395b7df3e
unknown <gabrielleite03@gmail.com>
2026-04-08 13:58:50 -0300

    created model cost center

---
+++ model/cost-center.go
@@ -0,0 +1,25 @@
+package model
+
+type CostCenter struct {
+	ID          string               `json:"id" db:"id"`
+	Name        string               `json:"name" db:"name"`
+	Code        string               `json:"code" db:"code"`
+	Description string               `json:"description" db:"description"`
+	Active      bool                 `json:"active" db:"active"`
+	Properties  []CostCenterProperty `json:"properties,omitempty"`
+}
+
+type CostCenterPropertyType string
+
+const (
+	CostCenterPropertyTypeIndex CostCenterPropertyType = "index"
+	CostCenterPropertyTypeValue CostCenterPropertyType = "value"
+)
+
+type CostCenterProperty struct {
+	ID           int64                  `json:"id" db:"id"`
+	CostCenterID string                 `json:"costCenterId" db:"cost_center_id"`
+	Name         string                 `json:"name" db:"name"`
+	Value        string                 `json:"value" db:"value"`
+	Type         CostCenterPropertyType `json:"type" db:"type"`
+}
